dbb09f9b6ae496dd6b760fd58a33b7bd5471809f
Ryan Noon <rmnoon@cs.stanford.edu>
2022-06-19 16:51:00 -0400

    removed compiler option

--- api/hello.ts
+++ api/hello.ts
@@ -1,5 +1,8 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
+/**
+ * See: https://vercel.com/docs/concepts/functions/serverless-functions/supported-languages#using-typescript
+ */
 export default (request: VercelRequest, response: VercelResponse) => {
 	const { name } = request.query;
 	response.status(200).send(`Hello ${name}!`);
