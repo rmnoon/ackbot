8b145480aa68214a557f61f2713d3337b7154a32
Ryan Noon <rmnoon@cs.stanford.edu>
2022-06-20 22:35:54 -0400

    ssl require

--- src/api/_constants.ts
+++ src/api/_constants.ts
@@ -15,7 +15,7 @@ export const sql = postgres({
 	database: process.env.PGDATABASE,
 	username: process.env.PGUSER,
 	password: process.env.PGPASSWORD,
-	ssl: false,
+	ssl: 'require',
 });
 
 export const slack = new WebClient(process.env.SLACK_BOT_TOKEN);
